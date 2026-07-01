2187b4a524e29b73bf3cb7eefd5621b03d7ee9ac
Suleman Gulzar <suleman_gulzar@icloud.com>
2026-07-02 00:36:33 +0500

    Updated

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -3,40 +3,35 @@ from uuid import UUID, uuid4
 
 import sqlalchemy.dialects.postgresql as pg
 from pydantic import EmailStr
-from sqlmodel import Field, SQLModel,Column
+from sqlmodel import Column, Field, SQLModel
 
 # Assuming app/db/base.py has: Base = SQLModel
 from app.db.base import Base
+
 
 class User(Base, table=True):
     __tablename__ = "users"
 
     id: UUID = Field(
         default_factory=uuid4,
-        sa_column=Column(pg.UUID(as_uuid=True), primary_key=True, nullable=False)
+        sa_column=Column(pg.UUID(as_uuid=True), primary_key=True, nullable=False),
     )
 
     name: str = Field(nullable=False)
 
-    email: EmailStr = Field(
-        nullable=False,
-        unique=True,
-        index=True
-    )
+    email: EmailStr = Field(nullable=False, unique=True, index=True)
 
     hashed_password: str = Field(nullable=False)
 
     is_active: bool = Field(default=True, nullable=False)
 
     created_at: datetime = Field(
-        default_factory=datetime.utcnow,
-        sa_column=Column(pg.TIMESTAMP(timezone=True)
+        default_factory=datetime.utcnow, sa_column=Column(pg.TIMESTAMP(timezone=True))
     )
-
     updated_at: datetime = Field(
         default_factory=datetime.utcnow,
         sa_column=Column(
             pg.TIMESTAMP(timezone=True),
-            onupdate=datetime.utcnow #
-        )
+            onupdate=datetime.utcnow,  #
+        ),
     )
